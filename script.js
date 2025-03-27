e0c0d9987a2ca3a407b1f7e90bceacaf594eacf5
vaish737 <115417170+vaish737@users.noreply.github.com>
2025-03-27 10:11:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function generatePassword() {
+    const length = document.getElementById("length").value;
+    const includeUppercase = document.getElementById("uppercase").checked;
+    const includeLowercase = document.getElementById("lowercase").checked;
+    const includeNumbers = document.getElementById("numbers").checked;
+    const includeSymbols = document.getElementById("symbols").checked;
+
+    const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const symbols = "!@#$%^&*()_+-={}[]|:;<>,.?/";
+
+    let characters = "";
+    if (includeUppercase) characters += uppercaseLetters;
+    if (includeLowercase) characters += lowercaseLetters;
+    if (includeNumbers) characters += numbers;
+    if (includeSymbols) characters += symbols;
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+    document.getElementById("password").value = password;
+}
+
+function copyPassword() {
+    const passwordField = document.getElementById("password");
+    passwordField.select();
+    document.execCommand("copy");
+    alert("Password copied to clipboard!");
+}
